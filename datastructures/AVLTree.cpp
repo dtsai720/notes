994e4cb516a6448e8a103a979ca2a9124c95cff5
davidtsai0720 <djuni0720@gmail.com>
2022-07-07 20:55:02 +0800

    minor change

--- datastructures/AVLTree.cpp
+++ datastructures/AVLTree.cpp
@@ -35,108 +35,119 @@ Right Left Rotate:
 class TreeNode {
 
 public:
-    int value;
-    TreeNode *left, *right;
-    TreeNode(int value) { this->value = value; }
+    int Value;
+    TreeNode *Left, *Right;
+    TreeNode(int value) { this->Value = value; }
 };
 
 class AVLTree {
+
 private:
-    int height(TreeNode* node);
+    int Height(TreeNode* node);
     TreeNode *LeftRotate(TreeNode *x);
     TreeNode *RightRotate(TreeNode *y);
     TreeNode *LeftRightRotate(TreeNode *node);
     TreeNode *RightLeftRotate(TreeNode *node);
 
 public:
-    TreeNode *insert(TreeNode *root, int value);
-    TreeNode *deleteNode(TreeNode *root, int value);
-    TreeNode *minValueNode(TreeNode *root);
-    TreeNode *balance(TreeNode *node);
-    int difference(TreeNode *node);
-    void show(TreeNode *root);
+    TreeNode *InsertNode(TreeNode *root, int value);
+    TreeNode *DeleteNode(TreeNode *root, int value);
+    TreeNode *MinValueNode(TreeNode *root);
+    TreeNode *Balance(TreeNode *node);
+    int Difference(TreeNode *node);
+    void Show(TreeNode *root);
 };
 
 inline TreeNode *AVLTree::LeftRotate(TreeNode *x) {
-    TreeNode *y = x->right;
-    x->right = y->left;
-    y->left = x;
+    TreeNode *y = x->Right;
+    x->Right = y->Left;
+    y->Left = x;
     return y;
 }
+
 inline TreeNode *AVLTree::RightRotate(TreeNode *y) {
-    TreeNode *x = y->left;
-    y->left = x->right;
-    x->right = y;
+    TreeNode *x = y->Left;
+    y->Left = x->Right;
+    x->Right = y;
     return x;
 }
+
 inline TreeNode *AVLTree::LeftRightRotate(TreeNode *z) {
-    TreeNode *x = z->left;
-    z->left = this->LeftRotate(x);
+    TreeNode *x = z->Left;
+    z->Left = this->LeftRotate(x);
     return this->RightRotate(z);
 }
+
 inline TreeNode *AVLTree::RightLeftRotate(TreeNode *z) {
-    TreeNode *x = z->right;
-    z->right = this->RightLeftRotate(x);
+    TreeNode *x = z->Right;
+    z->Right = this->RightLeftRotate(x);
     return this->LeftRotate(z);
 }
-inline int AVLTree::height(TreeNode *node) {
+
+inline int AVLTree::Height(TreeNode *node) {
     return node == nullptr? 0:
-        1+std::max(this->height(node->left), this->height(node->right));
+        1+std::max(this->Height(node->Left), this->Height(node->Right));
 }
-inline int AVLTree::difference(TreeNode *node) {
-    return this->height(node->left) - this->height(node->right);
+
+inline int AVLTree::Difference(TreeNode *node) {
+    return this->Height(node->Left) - this->Height(node->Right);
 }
-inline TreeNode *AVLTree::insert(TreeNode *root, int value) {
+
+inline TreeNode *AVLTree::InsertNode(TreeNode *root, int value) {
     if (root == nullptr)
         return new TreeNode(value);
-    if (root->value > value)
-        root->left = this->insert(root->left, value);
+    if (root->Value > value)
+        root->Left = this->InsertNode(root->Left, value);
     else
-        root->right = this->insert(root->right, value);
-    return this->balance(root);
+        root->Right = this->InsertNode(root->Right, value);
+    return this->Balance(root);
 }
-inline TreeNode *AVLTree::deleteNode(TreeNode *root, int value) {
+
+inline TreeNode *AVLTree::DeleteNode(TreeNode *root, int value) {
     if (root == nullptr)
         return nullptr;
-    if (root->value > value)
-        root->left = this->deleteNode(root->left, value);
-    else if (root->value < value)
-        root->right = this->deleteNode(root->right, value);
-    else if (root->left == nullptr)
-        return root->right;
-    else if (root->right == nullptr) {
-        return root->left;
+    if (root->Value > value)
+        root->Left = this->DeleteNode(root->Left, value);
+    else if (root->Value < value)
+        root->Right = this->DeleteNode(root->Right, value);
+    else if (root->Left == nullptr)
+        return root->Right;
+    else if (root->Right == nullptr) {
+        return root->Left;
     } else {
-        TreeNode *temp = this->minValueNode(root->right);
-        root->value = temp->value;
-        root->right = this->deleteNode(root->right, temp->value);
+        TreeNode *temp = this->MinValueNode(root->Right);
+        root->Value = temp->Value;
+        root->Right = this->DeleteNode(root->Right, temp->Value);
     }
-    return this->balance(root);
+    return this->Balance(root);
 }
-inline TreeNode *AVLTree::minValueNode(TreeNode *root) {
+
+inline TreeNode *AVLTree::MinValueNode(TreeNode *root) {
     TreeNode *cur;
-    for (cur = root; cur->left != nullptr; cur = cur->left);
+    for (cur = root; cur->Left != nullptr; cur = cur->Left);
     return cur;
 }
-inline TreeNode *AVLTree::balance(TreeNode *root) {
-    int bf = this->difference(root);
+
+inline TreeNode *AVLTree::Balance(TreeNode *root) {
+    int bf = this->Difference(root);
     if (std::abs(bf) <= 1)
         return root;
     if (bf > 1)
-        return this->difference(root->left) > 0? this->RightRotate(root): this->RightLeftRotate(root);
-    return this->difference(root->right) > 0? this->LeftRightRotate(root): this->LeftRotate(root);
+        return this->Difference(root->Left) > 0? this->RightRotate(root): this->RightLeftRotate(root);
+    return this->Difference(root->Right) > 0? this->LeftRightRotate(root): this->LeftRotate(root);
 }
-inline void AVLTree::show(TreeNode *root) {
+
+inline void AVLTree::Show(TreeNode *root) {
     if (root == nullptr) return;
     std::queue<TreeNode*> q;
     q.push(root);
     while (!q.empty()) {
         TreeNode *node = q.front(); q.pop();
-        std::cout << node->value << std::endl;
-        if (node->left != nullptr)
-            q.push(node->left);
-        if (node->right != nullptr)
-            q.push(node->right);
+        std::cout << node->Value << std::endl;
+        if (node->Left != nullptr)
+            q.push(node->Left);
+        if (node->Right != nullptr)
+            q.push(node->Right);
     }
 }
 
@@ -144,8 +155,8 @@ TEST(TestAVLTree, AVLTree) {
     TreeNode *root = new TreeNode(0);
     AVLTree solution = AVLTree();
     for (int i = 1; i < (1 << 4) - 1; i++)
-        root = solution.insert(root, i);
-    ASSERT_EQ(solution.difference(root), 0);
+        root = solution.InsertNode(root, i);
+    ASSERT_EQ(solution.Difference(root), 0);
     delete(root);
 }
 
--- datastructures/RBTree.cpp
+++ datastructures/RBTree.cpp
@@ -14,59 +14,59 @@
 */
 
 class TreeNode {
-public:
-    bool color;
-    int value;
-    TreeNode *left, *right, *parent;
 
-    TreeNode(int value) {
-        this->value = value;
-    }
+public:
+    bool Color;
+    int Value;
+    TreeNode *Left, *Right, *Parent;
+    TreeNode(int value) { this->Value = value; }
 };
 
 
 class RedBlackTree {
+
 private:
-    TreeNode *root, *neel;
+    TreeNode *Root, *Nil;
+
 public:
     void LeftRotate(TreeNode *x);
     void RightRotate(TreeNode *x);
 };
 
 inline void RedBlackTree::LeftRotate(TreeNode *x) {
-    TreeNode *y = x->right;
-    x->right = y->left;
-    if (y->left != this->neel)
-        y->left->parent = x;
+    TreeNode *y = x->Right;
+    x->Right = y->Left;
+    if (y->Left != this->Nil)
+        y->Left->Parent = x;
 
-    y->parent = x->parent;
-    if (x->parent == this->neel)
-        this->root = y;
-    else if (x == x->parent->left)
-        x->parent->left = y;
+    y->Parent = x->Parent;
+    if (x->Parent == this->Nil)
+        this->Root = y;
+    else if (x == x->Parent->Left)
+        x->Parent->Left = y;
     else
-        x->parent->right = y;
+        x->Parent->Right = y;
 
-    y->left = x;
-    x->parent = y;
+    y->Left = x;
+    x->Parent = y;
 }
 
 inline void RedBlackTree::RightRotate(TreeNode *y) {
-    TreeNode *x = y->left;
-    y->left = x->right;
-    if (x->right != this->neel)
-        x->right->parent = y;
+    TreeNode *x = y->Left;
+    y->Left = x->Right;
+    if (x->Right != this->Nil)
+        x->Right->Parent = y;
 
-    x->parent = y->parent;
-    if (x->parent == this->neel)
-        this->root = x;
-    else if (y == y->parent->left)
-        y->parent->left = x;
+    x->Parent = y->Parent;
+    if (x->Parent == this->Nil)
+        this->Root = x;
+    else if (y == y->Parent->Left)
+        y->Parent->Left = x;
     else
-        y->parent->right = x;
+        y->Parent->Right = x;
     
-    x->right = y;
-    y->parent = x;
+    x->Right = y;
+    y->Parent = x;
 }
 
 int main(int argc, char **argv) {
