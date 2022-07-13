d44a92276710b60f24f7fc2b556b9e717046ca83
davidtsai0720 <djuni0720@gmail.com>
2022-07-13 22:18:31 +0800

    minor change

--- leetcode/0473.cpp
+++ leetcode/0473.cpp
@@ -4,26 +4,30 @@
 
 class Solution {
 private:
-    bool backtrace(std::vector<int>& nums, std::vector<int>& arr, int idx) {
-        if (idx == nums.size())
-            return std::accumulate(arr.begin(), arr.end(), 0) == 0;
-        for (int side = 0; side < 4; side++) {
-            if (arr[side] < nums[idx]) continue;
-            arr[side] -= nums[idx];
-            if (backtrace(nums, arr, idx+1)) return true;
-            arr[side] += nums[idx];
-        }
-        return false;
-    }
+    bool backtrace(std::vector<int>& nums, std::vector<int>& arr, int idx);
 public:
-    bool makesquare(std::vector<int>& matchsticks) {
-        int total = std::accumulate(matchsticks.begin(), matchsticks.end(), 0);
-        if (total % 4 != 0) return false;
-        std::sort(matchsticks.rbegin(), matchsticks.rend());
-        std::vector<int> arr(4, total/4);
-        return backtrace(matchsticks, arr, 0);
-    }
+    bool makesquare(std::vector<int>& matchsticks);
 };
+
+bool Solution::backtrace(std::vector<int>& nums, std::vector<int>& arr, int idx) {
+    if (idx == nums.size())
+        return std::accumulate(arr.begin(), arr.end(), 0) == 0;
+    for (int side = 0; side < 4; side++) {
+        if (arr[side] < nums[idx]) continue;
+        arr[side] -= nums[idx];
+        if (backtrace(nums, arr, idx+1)) return true;
+        arr[side] += nums[idx];
+    }
+    return false;
+}
+
+bool Solution::makesquare(std::vector<int>& matchsticks) {
+    int total = std::accumulate(matchsticks.begin(), matchsticks.end(), 0);
+    if (total % 4 != 0) return false;
+    std::sort(matchsticks.rbegin(), matchsticks.rend());
+    std::vector<int> arr(4, total/4);
+    return backtrace(matchsticks, arr, 0);
+}
 
 TEST(TestMakeSquareSuite, TestMakeSquarePass) {
     std::vector<int> in{1,1,2,2,2};
