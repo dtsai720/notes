772771f5fb41da1ff07b152464e76073294f0d36
davidtsai0720 <djuni0720@gmail.com>
2022-07-20 12:02:34 +0800

    opt

--- leetcode/String/0792.cpp
+++ leetcode/String/0792.cpp
@@ -1,45 +1,27 @@
 #include <string>
-#include <map>
+#include <unordered_map>
 #include <gtest/gtest.h>
-
-class Trie {
-public:
-    char c;
-    Trie *next;
-    Trie(char c) {
-        this->c = c;
-        this->next = nullptr;
-    }
-};
 
 class Solution {
 private:
-    std::map<std::pair<Trie*, std::string>, bool> mymap;
-    bool isSubseq(Trie *head, std::string s);
+    bool isSubseq(std::string &s1, const std::string &s2);
 public:
     int numMatchingSubseq(std::string s, std::vector<std::string> words);
 };
 
-bool Solution::isSubseq(Trie *head, std::string s) {
-    if (this->mymap.count({head, s}) > 0) return this->mymap[{head, s}];
-    Trie *cur = head;
-    int idx = 0;
-    while (cur != nullptr && idx < s.length()) {
-        if (cur->c == s[idx]) idx++;
-        cur = cur->next;
-    }
-    return this->mymap[{head, s}] = idx == s.length();
+bool Solution::isSubseq(std::string &s1, const std::string &s2) {
+    size_t cur = 0;
+    for (size_t i = 0; i < s1.length() && cur < s2.length(); i++)
+        if (s1[i] == s2[cur]) cur++;
+    return cur == s2.length();
 }
 
 int Solution::numMatchingSubseq(std::string s, std::vector<std::string> words) {
     int result = 0;
-    Trie *root = new Trie('-');
-    Trie *cur = root;
-    for (char &c: s) {
-        cur->next = new Trie(c);
-        cur = cur->next;
-    }
-    for (std::string word: words) result += this->isSubseq(root, word);
+    std::unordered_map<std::string, int> mymap;
+    for (std::string word: words) mymap[word]++;
+    for (const std::pair<std::string, int> p: mymap)
+        result += this->isSubseq(s, p.first)? p.second: 0;
     return result;
 }
 
